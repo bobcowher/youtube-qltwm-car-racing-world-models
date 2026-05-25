bb2fd448d9555e467d8e446f20eeb2fc828fe675
Robert Cowher <bobcowher@gmail.com>
2026-05-25 10:16:53 -0500

    Building out the rest of the world model

--- models/world_model.py
+++ models/world_model.py
@@ -30,7 +30,86 @@ class WorldModel(BaseModel):
         self.n_actions = n_actions
 
 
+    def normalize_embedding(self, embed):
+        return self.embed_norm_layer(embed)
+
+    def encode(self, obs):
+        embed = self.encoder(obs)
+        embed = self.normalize_embedding(embed)
+        return embed
+
+    def decode(self, embeds):
+        return self.decoder(embeds)
+
+    def imagine_step(self, embed, action):
+
+        next_embed = self.dynamics(embed, action)
+        next_embed = self.normalize_embedding(next_embed)
+
+        embed_action = torch.cat([embed, action], dim=-1)
+
+        reward = self.reward_pred(embed_action)
+        done = torch.sigmoid(self.done_pred(embed_action))
+
+        return next_embed, reward, done
+
+    def forward(self, obs, action):
+
+        embeds = self.encode(obs)
+
+        embeds_flat = embeds.view(-1, embeds.shape[-1])
+        recon = self.decode(embeds_flat)
+
+        next_embed_pred = self.dynamics(embeds_flat, action)
+        next_embed_pred = self.normalize_embedding(next_embed_pred)
+
+        embed_action = torch.cat([embeds_flat, action], dim=-1)
+        reward_pred = self.reward_pred(embed_action)
+
+        done_pred = torch.sigmoid(self.done_pred(embed_action))
+
+        return recon, embeds, next_embed_pred, reward_pred, done_pred
+
+    def compute_loss(self, obs, actions, rewards, next_obs, dones):
+
+        obs_normalized = obs.float() / 255.0
+        next_obs_normalized = next_obs.float() / 255.0
+
+        if obs_normalized.ndim == 5:
+            obs_normalized = obs_normalized.squeeze(1)
+        if next_obs_normalized.ndim == 5:
+            next_obs_normalized = next_obs_normalized.squeeze(1)
+
+        action_onehot = F.one_hot(actions.long(), num_classes=self.n_actions).float()
+
+        recon, _, next_embed_pred, reward_pred, done_pred = self.forward(obs_normalized, action_onehot)
+
+        recon_loss = F.l1_loss(recon, obs_normalized) + 0.2 * ssim_loss(recon, obs_normalized) + 0.1 * gradient_loss(recon, obs_normalized)
+
+        next_embeds = self.encode(next_obs_normalized)
+        next_embed_target = next_embeds.view(-1, next_embeds.shape[-1])
+
+        dynamics_loss = F.mse_loss(next_embed_pred, next_embed_target.detach())
+
+        reward_loss = F.mse_loss(reward_pred.squeeze(-1), rewards.float())
         
+        done_loss = F.mse_loss(done_pred.squeeze(-1), dones.float())
+
+        combined_loss = (
+            1.0 * recon_loss +
+            1.0 * dynamics_loss +
+            2.0 * reward_loss +
+            0.5 * done_loss
+        )
+
+        return combined_loss, {
+            "total": combined_loss.item(),
+            "recon": recon_loss.item(),
+            "dynamics": dynamics_loss.item(),
+            "reward": reward_loss.item(),
+            "done": done_loss.item(),
+        }
+
 
 
 
