1cb2b54abff8a918be3db7c3a9f8cb3815fc4241
Robert Cowher <bobcowher@gmail.com>
2026-06-14 09:32:54 -0500

    Offline training epochs back to 100

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ env = gym.make("CarRacing-v3", continuous=False, render_mode="rgb_array")
 
 agent = Agent(env=env, max_buffer_size=200000, target_update_interval=10000)
 
-agent.train(episodes=1200, batch_size=32, wm_batch_size=32, offline_training_epochs=200, imagination_steps=4)
+agent.train(episodes=1200, batch_size=32, wm_batch_size=32, offline_training_epochs=100, imagination_steps=4)
 
 
